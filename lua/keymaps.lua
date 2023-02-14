53a4476b375d666735f7328ea6d6bf65a1cc50f8
= <xtriminioh@gmail.com>
2023-02-14 11:24:58 -0600

    configuracion base para Vim

---
+++ lua/keymaps.lua
@@ -0,0 +1,14 @@
+----------------------------------------------------------
+-- Keymaps configuration file: keymaps of neovim
+-- and plugins.
+-----------------------------------------------------------
+local map = vim.keymap.set
+local default_opts = {noremap = true, silent = true}
+local cmd = vim.cmd
+-----------------------------------------------------------
+
+----------------------------------------------------------
+-- Neovim shortcuts
+-----------------------------------------------------------
+
+map('n', '<leader>c', ':nohl<CR>', default_opts)
